0290f208a8fd2f68b177edbc47476ef2377d9781
Yuji Yamamoto <yujiyamamoto64@gmail.com>
2025-11-19 08:45:52 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/yujiyamamoto64/igni-code-challenge"
+  base: "/igni-code-challenge/"
 });
