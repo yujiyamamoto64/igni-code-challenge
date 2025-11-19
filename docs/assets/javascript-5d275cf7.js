e825a98f3d09f3832aa4349906ff506daa77016c
Yuji Yamamoto <yujiyamamoto64@gmail.com>
2025-11-19 09:37:07 -0300

    Atualizei a configuração do Vite para gerar diretamente o conteúdo da página estática no diretório docs

--- docs/assets/javascript-5d275cf7.js
+++ docs/assets/javascript-5d275cf7.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-e41e34fe.js";import"./index-24cdecf2.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-c4d222af.js";import"./index-d13dd223.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.47.0(69991d66135e4a1fc1cf0b1ac4ad25d429866a0d)
  * Released under the MIT license
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/igni-code-challenge/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
